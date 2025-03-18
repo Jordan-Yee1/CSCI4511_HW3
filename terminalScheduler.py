84d1363b9c1d6b85ddaa0a6937d47a7d4e12d7e6
Jordan Yee <11jor@Jordans-Computer.local>
2025-03-18 00:39:21 -0400

    Wow Ive been spelling Hangar like Hanger this whole time, finished up some classes (for now) time to get to scheduling (it's doomed)

--- terminalScheduler.py
+++ terminalScheduler.py
@@ -10,10 +10,40 @@ import sys
 '''
 Variables:
 Arrivals/Aircraft
-Hangers
+Hangars
 Forklifts
 Trucks
 
+{
+    "aircraft": {
+        "The Aircraft": {
+            "Hangar": "The Hangar",
+            "Arrival": 800,
+            "Departure": 820
+        }
+    },
+    "trucks": {
+        "The Truck": {
+            "Hangar": "The Hangar",
+            "Arrival": 820,
+            "Departure": 825
+        }
+    },
+    "forklifts": {
+        "The Forklift": [
+            {
+                "Hangar": "The Hangar",
+                "Time": 800,
+                "Job": "Unload"
+            },
+            {
+                "Hangar": "The Hangar",
+                "Time": 825,
+                "Job": "Load"
+            }
+        ]
+    }
+}
 
 '''
 
@@ -32,42 +62,78 @@ class Aircraft:
 
 
 
-class Hanger:
-    def __init__(self, id, Aircraft, Truck, pallets):
+class Hangar:
+    def __init__(self, id):
         self.id = id
-        self.aircraft = Aircraft
-        self.pallets = pallets
+        self.aircraft = None
+        self.pallets = 0
+        self.trucks = []
+
+    def addPlane(self, aircraft):
+        self.aircraft = aircraft
+
+    def addTruck(self, truck):
+        self.trucks.append(truck)
+
+    def __str__(self):
+        return f'\nHangar id : {self.id} | Aircraft : {self.aircraft} | Pallets : {self.pallets} | Trucks : {self.trucks}'  
+    
+    def __repr__(self):
+        return self.__str__()
 
 class Forklifts:
-    def __init__(self, id, Aircraft, Hanger, Pallet, Truck):
+    def __init__(self, id,):
         self.id = id
-        self.aircraft = Aircraft
-        self.hanger = Hanger
-        self.pallet = Pallet
-        self.truck = Truck
+        self.schedule = []
+
+    def add_job(self, hangar, time, job):
+        newJob = {
+            "Hangar": hangar,   #ID of hangar
+            "Time": time,       #time in format
+            "Job": job          #Either load or unload
+            }
+        
+        self.schedule.append(newJob)
+    
+    def __str__(self):
+        return f'\nForklift id : {self.id} | Schedule : {self.schedule}' 
+    
+    def __repr__(self):
+        return self.__str__()
+
 
 class Truck:
-    def __init__(self, id, Forklifts, Hanger):
+    def __init__(self, id, Time):
         self.id = id
-        self.forklifts = Forklifts
-        self.hanger = Hanger
+        self.time = Time      
+        self.Hangar = None
+    
+    def __str__(self):
+        return f'\nTruck id : {self.id} | Arrival Time : {self.time} | Hangar : {self.Hangar}'  
+    
+    def __repr__(self):
+        return self.__str__()
+
 
 
 class Meta:
-    def __init__(self, start, stop, hangers, forklifts)
+    def __init__(self, start, stop, Hangars, forklifts):
         self.start = start
         self.stop = stop
-        self.hangers = hangers
+        self.Hangars = Hangars
         self.forklifts = forklifts
+    
+
+    def __str__(self):
+        return f'\n Meta Start: {self.start} | Stop Time : {self.stop} | Hangars : {self.Hangars}, Forklifts : {self.forklifts}'  
+    
+    def __repr__(self):
+        return self.__str__()
    
-    
-    
 #I think it passes it in as a dict in a dict, dictception???
-class Parser:
-    
+class Parser: 
     def parseAircraft(self, jsonData):
         data = json.loads(jsonData)
-        print(data)
         ret_aircraft = []
         for aircraft, values in data.items():
             new_aircraft = Aircraft(aircraft, values['Time'], values['Cargo'])
@@ -76,12 +142,24 @@ class Parser:
     
     def parseMeta(self, jsonData):
         data = json.loads(jsonData)
-        ret_data = {
-            "start": data['StartT Time'],
-            "stop" : data['Stop Time'],
-            ""
+        ret_data = Meta(
+            start = data['Start Time'],
+            stop = data['Stop Time'],
+            Hangars = data['Hangars'],
+            forklifts = data['Forklifts']
+        )
+        return ret_data
+    
+    def parseTruck(self, jsonData):
+        data = json.loads(jsonData)
+        ret_truck = []
+        for truck, values, in data.items():
+            new_truck = Truck(truck, values)
+            ret_truck.append(new_truck)
+        return ret_truck
+    
+    
 
-        }
 
     
 def read_file(filename):
@@ -90,6 +168,7 @@ def read_file(filename):
     
 
 # terminalScheduler.py META_PATH AIRCRAFT_PATH TRUCKS_PATH SCHEDULE_PATH
+# python terminalScheduler.py ./tests/test1_passes/meta.json ./tests/test1_passes/aircraft.json ./tests/test1_passes/trucks.json ./
 def main():
     parse = Parser()
 
@@ -102,7 +181,7 @@ def main():
 
 
     trucks = read_file(sys.argv[3])
-    print(parse.parseTrucks(trucks))
+    print(parse.parseTruck(trucks))
 
     #AND ALSO OUTPUT FILE SOMEWHERE 
     #WRITE TO SCHEDULE PATH OR SMTHN
